345ec1caebee33391d72ca7ef82cd9d7ed1017d8
Shawn Mclean <mixmasterxp@gmail.com>
2012-10-13 18:06:15 -0500

    Added CRUD layout from Cards

--- src/Cards.cs
+++ src/Cards.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grikly.Models;
 
 namespace Grikly
 {
     public partial class GriklyApi
     {
+        public Card GetCard(int cardId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card CreateCard(Card card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card UpdateCard(Card card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card DeleteCard(int cardId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
