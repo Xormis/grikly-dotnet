75c1eac12f726a49aca1c816d9b1371c0ba4f4e8
Shawn Mclean <mixmasterxp@gmail.com>
2012-10-13 17:56:13 -0500

    added docs

--- src/Accounts.cs
+++ src/Accounts.cs
@@ -9,11 +9,23 @@ namespace Grikly
 {
     public partial class GriklyApi
     {
+        /// <summary>
+        /// Get a valid User by email and password
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public User GetValidUser(string email, string password)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get a valid user by userId and password
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public User GetValidUser(int userId, string password)
         {
             throw new NotImplementedException();
