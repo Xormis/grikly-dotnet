f3fe5f6009faec10357570b4da0b39eb4cdb6bca
Shawn Mclean <mixmasterxp@gmail.com>
2012-10-15 23:37:44 -0500

    added bs tests

---
+++ tests/IntegrationTests/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Newtonsoft.Json;
+
+namespace Grikly.Tests.IntegrationTests
+{
+    public class lol
+    {
+        public string test { get; set; }
+        public string test1 { get; set; }
+    }
+    [TestFixture]
+    public class Test
+    {
+
+        [Test]
+        public void test()
+        {
+            string l = "{test2:\"lol\", test1:\"val for test1\"}";
+
+            lol fsdfs = JsonConvert.DeserializeObject<lol>(l);
+
+        }
+    }
+}
