c5981af8e16aeb73e9ac31a43e3dcbf4cc77754b
Shawn Mclean <mixmasterxp@gmail.com>
2013-07-08 15:43:36 -0500

    removed console app

--- tests/IntegrationTests/UserTests.cs
+++ tests/IntegrationTests/UserTests.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using NUnit.Framework;
 
 namespace Grikly.Tests.IntegrationTests
@@ -10,6 +11,34 @@ namespace Grikly.Tests.IntegrationTests
     [TestFixture]
     public class UserTests
     {
+        private string apiKey;
+        private string userEmail;
+        private string userPassword;
+
+        [SetUp]
+        public void SetUp()
+        {
+            apiKey = ConfigurationManager.AppSettings["APIKey"];
+            userEmail = ConfigurationManager.AppSettings["UserEmail"];
+            userPassword = ConfigurationManager.AppSettings["UserPassword"];
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+        [Test]
+        public void Profile_Image_Can_Be_Uploaded()
+        {
+            var api = new GriklyApi(apiKey);
+            api.AddValidUserCredentials(userEmail,userPassword);
+            var userId = int.Parse(ConfigurationManager.AppSettings["UserId"]);
+
+            //api.UploadProfileImage(userId, new byte[] {}, "image", CancellationToken.None);
+        }
+
 
     }
 }
